4d86aec30795fa0947bbe44ab852cbfbd8b2f0cd
nqq <nqq@aozsky.com>
2020-10-26 21:11:19 +0800

    feat(domain/article): commit

---
+++ domain/article.go
@@ -0,0 +1,19 @@
+// Package domain provides ...
+package domain
+
+//PublicStatus 文章发布状态
+type PublicStatus int8
+
+const (
+	//Draft 草稿
+	Draft PublicStatus = iota
+	//Public 已发布
+	Public
+)
+
+//Article 文章实体
+type Article struct {
+	Title   string
+	Content string
+	Status  PublicStatus
+}
