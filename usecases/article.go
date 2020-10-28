75e5d0096b4f3593d95468cc5d61395942d8c277
bantana <bantana@aozsky.com>
2020-10-29 02:38:33 +0800

    fix(spell error): commit

--- usecases/article.go
+++ usecases/article.go
@@ -1,34 +1,36 @@
-// Package uscecases provides ...
-package uscecases
+// Package usecases provides ...
+package usecases
+
+import "iohttps.com/live/realworld-medium-rewrite/domain"
 
 //ArticleInteractor article interactor
 type ArticleInteractor struct {
 	ArticleRepo domain.ArticleRepository
 }
 
-//SaveDraft 保存草稿
-func(itor ArticleInteractor)SaveDraft
+// SaveDraft 保存草稿
+func (itor ArticleInteractor) SaveDraft()
 
-//Public 发布文章
-func(itor ArticleInteractor)Public
+// Public 发布文章
+func (itor ArticleInteractor) Public()
 
-//GetAuthorDrafts 获取作者的草稿列表
-func(itor ArticleInteractor)GetAuthorDrafts
+// GetAuthorDrafts 获取作者的草稿列表
+func (itor ArticleInteractor) GetAuthorDrafts()
 
-//GetAuthorPublicArticles 获取作者的已发布文章
-func(itor ArticleInteractor)GetAuthorPublicArticles
+// GetAuthorPublicArticles 获取作者的已发布文章
+func (itor ArticleInteractor) GetAuthorPublicArticles()
 
-//GetAllPublicArticles 获取所有已发布文章
-func(itor ArticleInteractor)GetAllPublicArticles
+// GetAllPublicArticles 获取所有已发布文章
+func (itor ArticleInteractor) GetAllPublicArticles()
 
-//GetPublicArticleDraft 获取对修改已发布文章编辑的草稿
-func(itor ArticleInteractor)GetPublicArticleDraft
+// GetPublicArticleDraft 获取对修改已发布文章编辑的草稿
+func (itor ArticleInteractor) GetPublicArticleDraft()
 
-//SavePublicArticleDraft 保存已发布文章草稿
-func(itor ArticleInteractor)SavePublicArticleDraft
+// SavePublicArticleDraft 保存已发布文章草稿
+func (itor ArticleInteractor) SavePublicArticleDraft()
 
-//PublishPublicArticleDraft 发布对已发布文章的修改草稿
-func(itor ArticleInteractor)PublishPublicArticleDraft
+// PublishPublicArticleDraft 发布对已发布文章的修改草稿
+func (itor ArticleInteractor) PublishPublicArticleDraft()
 
-//GenerateUUID
-func GenerateUUID() domain.NUUID 
+// GenerateUUID ...
+func GenerateUUID() domain.NUUID
