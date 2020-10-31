d68b805133ffc25e4d2214163b75927fcc135c1d
nqq <nqq@aozsky.com>
2020-10-31 09:39:11 +0800

    feat(user): commit
    user 的设计场景

--- domain/user.go
+++ domain/user.go
@@ -1,10 +1,15 @@
 // Package domain provides ...
 package domain
 
-//User
+//User ....
 type User struct {
 	ID       NUUID
 	Name     string
 	Email    string
 	Password string
+}
+
+//UserRepository ....
+type UserRepository interface {
+	/* TODO: add methods */
 }
