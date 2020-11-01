4dfe1334786eb11da0a1f6e09e1cb38e9359a4dc
nqq <nqq@aozsky.com>
2020-11-01 15:44:09 +0800

    feat(usecases/user.go): commit
    用户登录用例推导

--- usecases/user.go
+++ usecases/user.go
@@ -7,4 +7,23 @@ type UserInteractor struct {
 	UserRepo domain.UserRepository
 }
 
+//Token ...
+type Token struct {
+	ID     SUUID
+	UserID domain.NUUID
+}
+
+//Register 用户注册
 func (itor UserInteractor) Register()
+
+//Login 用户登录
+func (itor UserInteractor) Login()
+
+//CheckIdentityByEmail 通过email来校验身份
+func (itor UserInteractor) CheckIdentityByEmail()
+
+//CheckLoginStatus 校验用户是否登录
+func (itor UserInteractor) CheckIfLoggedin()
+
+//getUserByToken 通过token获取用户身份
+func (itor UserInteractor) getUserByToken()
