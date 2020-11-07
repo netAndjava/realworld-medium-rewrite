965b446d9c3c9e793dae21241dc3e11ae6698e97
nqq <nqq@aozsky.com>
2020-11-07 16:02:49 +0800

    feat(usecases/comment.go): commit
    添加作者或其他用户回复评论和作者删除评论用例

--- domain/comment.go
+++ domain/comment.go
@@ -4,6 +4,7 @@ package domain
 //Comment 文章评论
 type Comment struct {
 	ID        NUUID
+	PID       NUUID  //父ID，用来知道回复的评论
 	ArticleID NUUID  //评论的文章
 	Content   string //Content 评论的内容
 	Creator   User   //评论的创建者
--- usecases/comment.go
+++ usecases/comment.go
@@ -13,3 +13,9 @@ func (itor CommentInteractor) Add()
 
 //DropByCreator 评论长作者删除评论
 func (itor CommentInteractor) DropByCreator()
+
+//GetCommentsOfArticle 获取文章的评论列表
+func (itor CommentInteractor) GetCommentsOfArticle()
+
+//DropByArticleAuthor 文章作者删除针对文章的评论
+func (itor CommentInteractor) DropByArticleAuthor()
