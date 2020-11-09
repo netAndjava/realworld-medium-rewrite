b970addf12fad1f21ff8d3561b3f852960699268
nqq <nqq@aozsky.com>
2020-11-09 22:15:03 +0800

    feat(usecases/README.md): commit
    用户快捷登录

--- domain/user.go
+++ domain/user.go
@@ -3,11 +3,10 @@ package domain
 
 //User ....
 type User struct {
-	ID         NUUID
-	Name       string
-	Email      string
-	Password   string
-	IfVerified bool //邮箱是否校验过
+	ID       NUUID
+	Name     string
+	Email    string
+	Password string
 }
 
 //UserRepository ....
