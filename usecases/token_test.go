2c92e1ba04f295352b4df5c5c09de1a86cee12e0
bantana <bantana@aozsky.com>
2020-12-10 12:16:17 +0800

    docs(usecases/token_test.go): 命名和UI/UX中用户识别和用户体验中的影响 commit

--- usecases/token_test.go
+++ usecases/token_test.go
@@ -16,10 +16,21 @@ func TestLogin(t *testing.T) {
 	a.Nil(err)
 }
 
+// TODO:  login, logout, logged in, logged out<10-12-20, bantana> //
+// Noun: login, logout, loggedIn, loggedOut
+// Verb: log in, log out, logged in, logged out
+// Derived forms: logins, logged in, logging in, logs in
+// 重新理解命名的原因:
+// UI/UX中用户识别和用户体验中的影响
+// 1. sign on, sign in, sign out
+// 2. register, login, logout
+// 3. register, sign in, sign out (best)
+
+// TestCheckIfLoggedin tokenItor.CheckIfLogged 改为tokenItor.IsLoggedIn or tokenItor.IsOnline ?
 func TestCheckIfLoggedin(t *testing.T) {
 	a := assert.New(t)
 	tokenID := ""
-	_, err := tokenItor.CheckIfLoggedin(SUUID(tokenID))
+	_, err := tokenItor.CheckIfLogged(SUUID(tokenID))
 	a.NotNil(err)
 	tokenID = "test"
 	_, err := tokenItor.CheckIfLoggedin(SUUID(tokenID))
