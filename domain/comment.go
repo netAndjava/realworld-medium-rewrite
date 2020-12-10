dfa73243443b310bfc8ac2cf3f27f702591ed153
nqq <nqq@aozsky.com>
2020-12-10 15:49:20 +0800

    feat(domain): commit
    给 comment添加回复的ReplyUser知道回复的评论是谁

--- domain/comment.go
+++ domain/comment.go
@@ -6,6 +6,7 @@ type Comment struct {
 	ID        NUUID
 	PID       NUUID  //父ID，用来知道回复的评论
 	ArticleID NUUID  //评论的文章
+	ReplyUser NUUID  //回复的用户ID
 	Content   string //Content 评论的内容
 	Creator   User   //评论的创建者
 }
