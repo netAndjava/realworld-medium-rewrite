7698bc4f9c59e735977d7efee515ec07a36a1ac4
nqq <nqq@aozsky.com>
2020-12-12 14:44:06 +0800

    feat(infrastructure/mysql): commit
    实现mysql存储

---
+++ infrastructure/database/mysql/mysql.go
@@ -0,0 +1,105 @@
+// Package mysql provides ...
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"iohttps.com/live/realworld-medium-rewrite/infrastructure/database"
+)
+
+//MysqlHandler handler of mysql
+type mysqlHandler struct {
+	Conn *sql.DB
+}
+
+//NewMysqlHandler new mysqlHandler
+func NewMysqlHandler(dataSourceName string) (database.DbHandler, error) {
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	// TODO:理解为什么最大连接数设置为1时想要链接数据库获取数据时会获取不到任何结果  <21-10-20, nqq> //
+	// TODO:当道配置文件中配置  <21-10-20, nqq> //
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(time.Duration(4 * time.Hour))
+	return &mysqlHandler{db}, err
+}
+
+//Execute .....
+func (handler *mysqlHandler) Execute(query string, args ...interface{}) (database.Result, error) {
+	result, err := handler.Conn.Exec(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return &mysqlResult{result}, nil
+}
+
+//Query .....
+func (handler *mysqlHandler) Query(query string, args ...interface{}) (database.Rows, error) {
+	rows, err := handler.Conn.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &mysqlRows{rows}, nil
+}
+
+//QueryRow ....
+func (handler *mysqlHandler) QueryRow(query string, args ...interface{}) database.Row {
+	row := handler.Conn.QueryRow(query, args...)
+	return &mysqlRow{row}
+}
+
+//Ping ....
+func (handler *mysqlHandler) Ping() error {
+	return handler.Conn.Ping()
+}
+
+//PingContext ....
+func (handler *mysqlHandler) PingContext(ctx context.Context) error {
+	return handler.Conn.PingContext(ctx)
+}
+
+//mysqlResult result
+type mysqlResult struct {
+	Result sql.Result
+}
+
+//LastInsertId ....
+func (result *mysqlResult) LastInsertId() (int64, error) {
+	return result.Result.LastInsertId()
+}
+
+//RowsAffected ....
+func (result *mysqlResult) RowsAffected() (int64, error) {
+	return result.Result.RowsAffected()
+}
+
+//mysqlRows rows
+type mysqlRows struct {
+	Rows *sql.Rows
+}
+
+//Scan ....
+func (rows *mysqlRows) Scan(dest ...interface{}) error {
+	return rows.Rows.Scan(dest...)
+}
+
+//Next ...
+func (rows *mysqlRows) Next() bool {
+	return rows.Rows.Next()
+}
+
+type mysqlRow struct {
+	Row *sql.Row
+}
+
+//Scan ...
+func (row *mysqlRow) Scan(dest ...interface{}) error {
+	return row.Row.Scan(dest...)
+}
