7bec7d14f00b2e78f69afa9bd8550782f0a7f6e2
nqq <nqq@aozsky.com>
2020-12-13 19:33:38 +0800

    feat(infrastructure): commit
    实现api

---
+++ service/proto/protoc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "$# arguments"
+protoPath="./"
+out="../api"
+
+if [ -n "$1" ]; then
+    echo "input proto $1"
+else
+    echo "please input proto file"
+fi
+
+if [ -n "$2" ]; then
+   echo "input out_path $2" 
+   out=$2
+fi
+
+if [ -n "$3" ]; then
+   echo "input proto_path $3" 
+   protoPath=$3
+fi
+
+echo "protoc --proto_path=$protoPath --go_out=plugins=grpc:$out $1"
+protoc --proto_path=$protoPath --go_out=plugins=grpc:$out $1
