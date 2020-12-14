df7c41ef368071e86846ce984b4e8beb918d237a
nqq <nqq@aozsky.com>
2020-12-14 15:21:25 +0800

    feat(api): commit
    实现保存文章接口

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/stretchr/testify v1.6.1
+	google.golang.org/grpc v1.34.0
 )
--- service/action/article.go
+++ service/action/article.go
@@ -1,2 +1,45 @@
 // Package action provides ...
 package action
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "iohttps.com/live/realworld-medium-go/service/api"
+	"iohttps.com/live/realworld-medium-rewrite/domain"
+	"iohttps.com/live/realworld-medium-rewrite/usecases"
+)
+
+type articleServer struct {
+	pb.UnimplementedArticleServer
+	artInteractor usecases.ArticleInteractor
+}
+
+func Start() {
+	port := flag.String("port", ":8080", "please input port")
+	flag.Parse()
+	lis, err := net.Listen("tcp", *port)
+	if err != nil {
+		log.Fatalf("listen port:%d err:%v\n", port, err)
+	}
+	s := grpc.NewServer()
+
+	// TODO:  <14-12-20, nqq> //
+	var artItor usecases.ArticleInteractor
+
+	pb.RegisterArticleServer(s, &articleServer{ArtInteractor: artItor})
+}
+
+func (server *articleServer) SaveArticle(ctxt context.Context, art *pb.Article) (*pb.SaveArticle, error) {
+	if art.Id == 0 {
+		ID, err := server.artInteractor.CreateDraft(usecases.GenerateUUID, domain.Article{ID: art.Id, Title: art.Title, Content: art.Content, Status: domain.Draft, AuthorID: art.AuthorID}, art.AuthorID)
+		return pb.SaveArticle{Id: ID}, err
+	}
+
+	err := server.artInteractor.SaveDraft(domain.Article{ID: art.Id, Title: art.Title, Content: art.Content, Status: domain.Draft, AuthorID: art.AuthorID}, art.AuthorID)
+
+	return pb.SaveArticle{Id: art.Id}, err
+}
