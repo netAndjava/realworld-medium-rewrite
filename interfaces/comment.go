1e821a66d4b388cf7e38bd8174c572bf5c1815fc
nqq <nqq@aozsky.com>
2020-12-16 19:58:58 +0800

    feat(interfaces): commit
    实现通过pid查找评论列表

--- interfaces/comment.go
+++ interfaces/comment.go
@@ -24,3 +24,22 @@ func (repo *CommentRepo) Get(ID domain.NUUID) (domain.Comment, error) {
 	err := row.Scan(&c.PID, &c.ArticleID, &c.Content, &c.Creator)
 	return c, err
 }
+
+func (repo *CommentRepo) GetCommentByPID(PID domain.NUUID) ([]domain.Comment, error) {
+	rows, err := repo.Handler.Query(`select id,articleID,content,userID from t_comment where pid=?`, PID)
+	if err != nil {
+		return []domain.Comment{}, err
+	}
+	var comments []domain.Comment
+	for rows.Next() {
+		var c domain.Comment
+		err := rows.Scan(&c.ID, &c.ArticleID, &c.Content, &c.Creator)
+		if err != nil {
+			// TODO:记录错误日志  <16-12-20, nqq> //
+			continue
+		}
+		c.PID = PID
+		comments = append(comments, c)
+	}
+	return comments, nil
+}
