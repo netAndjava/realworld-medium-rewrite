fcd536744653a27098d2c291a7c7cc45b7dd27a8
nqq <nqq@aozsky.com>
2020-12-16 22:55:50 +0800

    feat(api): commit add start

--- service/comment/comment.go
+++ service/comment/comment.go
@@ -2,8 +2,11 @@
 package comment
 
 import (
+	"fmt"
 	"log"
+	"net"
 
+	"google.golang.org/grpc"
 	"iohttps.com/live/realworld-medium-rewrite/interfaces"
 	pb "iohttps.com/live/realworld-medium-rewrite/service/api"
 	"iohttps.com/live/realworld-medium-rewrite/usecases"
@@ -14,11 +17,38 @@ type commentServer struct {
 	commentItor usecases.CommentInteractor
 }
 
+// {
+// 	handler, err := mysql.NewMysqlHandler("root@/real_world_medium?charset=utf8")
+// 	if err != nil {
+// 		log.Fatal("connect database err:", err)
+// 	}
+//
+// }
+// // started is start service for comment
+// func  started(ip:port, dbhandle){
+// 	// 1.  init environment variables listen{ip:port}
+//
+// 	conn,err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+// 	// 1.1 db
+// 	repo = interfaces.NewCommentRepo(dbhandler)
+//
+// }
+
+//Start ....
 func Start(port int) {
-	handler, err := mysql.NewMysqlHanelr("root@/real_world_medium?charset=utf8")
-	if err != nil {
-		log.Fatal("connect database err:", err)
-	}
+
+	// handler, err := mysql.NewMysqlHandler("root@/real_world_medium?charset=utf8")
+	// if err != nil {
+	// 	log.Fatal("connect database err:", err)
+	// }
 	repo := interfaces.NewCommentRepo(handler)
+
+	conn, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("listen to port:%d err:%v\n", port, err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterService(server, &commentServer{usecases.CommentInteractor{repo}})
+	server.Serve(conn)
 
 }
