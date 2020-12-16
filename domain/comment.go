e7b9376333325932038972952d60ba45203a388f
nqq <nqq@aozsky.com>
2020-12-16 16:11:17 +0800

    feat(domain): commit
    重构评论，删除回复评论作者，@评论作者也是评论的内容

--- domain/comment.go
+++ domain/comment.go
@@ -5,15 +5,14 @@ import "errors"
 
 //Comment 文章评论
 // 文章在文章下面直接平路是楼主评论
-// 在楼主中回复评论，回复的是楼主评论，需要知道回复哪一楼
+// 在楼主中回复评论，回复的是楼主评论
 // 如果有@评论作者,则需要知道回复的评论人
 type Comment struct {
 	ID        NUUID
-	PID       NUUID   //父ID，用来知道回复的评论
-	Article   Article //评论的文章
-	ReplyUser User    //回复的用户ID
-	Content   string  //Content 评论的内容
-	Creator   User    //评论的创建者
+	PID       NUUID  //父ID，用来知道回复的评论
+	ArticleID NUUID  //评论的文章
+	Content   string //Content 评论的内容
+	Creator   UserID //评论的创建者
 }
 
 func (c Comment) Check() error {
