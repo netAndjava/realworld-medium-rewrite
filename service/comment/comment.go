470c4117ed93f28b39b2dbc7b99189e8d691957a
nqq <nqq@aozsky.com>
2020-12-17 09:28:20 +0800

    feat(api): commit 接口层实现添加评论

--- service/comment/comment.go
+++ service/comment/comment.go
@@ -2,11 +2,14 @@
 package comment
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"iohttps.com/live/realworld-medium-rewrite/domain"
+	"iohttps.com/live/realworld-medium-rewrite/infrastructure/database/mysql"
 	"iohttps.com/live/realworld-medium-rewrite/interfaces"
 	pb "iohttps.com/live/realworld-medium-rewrite/service/api"
 	"iohttps.com/live/realworld-medium-rewrite/usecases"
@@ -37,10 +40,10 @@ type commentServer struct {
 //Start ....
 func Start(port int) {
 
-	// handler, err := mysql.NewMysqlHandler("root@/real_world_medium?charset=utf8")
-	// if err != nil {
-	// 	log.Fatal("connect database err:", err)
-	// }
+	handler, err := mysql.NewMysqlHandler("root@/real_world_medium?charset=utf8")
+	if err != nil {
+		log.Fatal("connect database err:", err)
+	}
 	repo := interfaces.NewCommentRepo(handler)
 
 	conn, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -50,5 +53,14 @@ func Start(port int) {
 	server := grpc.NewServer()
 	pb.RegisterService(server, &commentServer{usecases.CommentInteractor{repo}})
 	server.Serve(conn)
+}
+
+//Add 添加评论
+func (server commentServer) Add(ctx context.Context, c *pb.Comment) (*pb.AddRep, error) {
+	ID, err := server.commentItor.Add(usecases.GenerateUUID, domain.Comment{PID: domain.NUUID(c.Pid, ArticleID), ArticleID: domain.NUUID(c.ArticleID), Content: c.Content, Creator: domain.NUUID(c.UserID)}, domain.NUUID(c.UserID))
+	if err != nil {
+		return nil, err
+	}
+	return &pb.AddRep{Id: int64(ID)}, nil
 
 }
