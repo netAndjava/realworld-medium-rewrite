2daa79c19dfa1c1e8b09617099a928d678f5a57b
nqq <nqq@aozsky.com>
2021-06-30 16:05:19 +0800

    feat(service): commit refactor

--- service/article/articleService.go
+++ service/article/articleService.go
@@ -11,15 +11,20 @@ import (
 
 type ArticleService interface {
 	Write(ctx context.Context, a domain.Article) (domain.NUUID, error)
+	ViewDraftArticles(ctx context.Context, userID domain.NUUID) ([]domain.Article, error)
+	View(ctx context.Context, ID domain.NUUID) (domain.Article, error)
+	Publish(ctx context.Context, a domain.Article) error
+	ViewPublicArticles(ctx context.Context, userID domain.NUUID) ([]domain.Article, error)
+	ViewRecentArticles(ctx context.Context) ([]domain.Article, error)
+	ViewDraftOfPublicArticle(ctx context.Context, ID domain.NUUID) (domain.Article, error)
+	Republish(ctx context.Context, a domain.Article) error
+	Drop(ctx context.Context, ID, userID domain.NUUID) error
 }
 
 type articleService struct {
 	Itor   usecases.ArticleInteractor
 	logger log.Logger
 }
-
-// find a bug
-// 你先手动吧
 
 func NewArticleService(logger log.Logger, itor usecases.ArticleInteractor) ArticleService {
 	return &articleService{Itor: itor, logger: logger}
@@ -39,4 +44,40 @@ func (as *articleService) Write(ctx context.Context, a domain.Article) (domain.N
 
 	err := as.Itor.EditPublicArticle(a)
 	return a.ID, err
+}
+
+func (as *articleService) ViewDraftArticles(ctx context.Context, userID domain.NUUID) ([]domain.Article, error) {
+	return as.Itor.ViewDraftArticles(userID)
+}
+
+func (as *articleService) View(ctx context.Context, ID domain.NUUID) (domain.Article, error) {
+	return as.Itor.View(ID)
+}
+
+func (as *articleService) Publish(ctx context.Context, a domain.Article) error {
+	_, err := as.Write(ctx, a)
+	if err != nil {
+		return err
+	}
+	return as.Itor.Publish(a.ID, a.AuthorID)
+}
+
+func (as *articleService) ViewPublicArticles(ctx context.Context, userID domain.NUUID) ([]domain.Article, error) {
+	return as.Itor.ViewPublicArticles(userID)
+}
+
+func (as *articleService) ViewRecentArticles(ctx context.Context) ([]domain.Article, error) {
+	return as.Itor.ViewRecentArticles()
+}
+
+func (as *articleService) ViewDraftOfPublicArticle(ctx context.Context, ID domain.NUUID) (domain.Article, error) {
+	return as.Itor.ViewDraftOfPublicArticle(ID)
+}
+
+func (as *articleService) Republish(ctx context.Context, a domain.Article) error {
+	return as.Itor.Republish(a)
+}
+
+func (as *articleService) Drop(ctx context.Context, ID, userID domain.NUUID) error {
+	return as.Itor.Drop(ID, userID)
 }
