303514beee0cd5eeba67b063037bbd92cb124750
nqq <nqq@aozsky.com>
2021-07-13 10:44:59 +0800

    up

--- articles/labs/build/build.sh
+++ articles/labs/build/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -ex
-buildah unshare sh /home/bantana/go/src/iohttps.com/live/realworld-medium-rewrite/article/labs/build/build-article-script.sh
+buildah unshare sh /home/bantana/go/src/iohttps.com/live/realworld-medium-rewrite/articles/labs/build/build-article-script.sh
