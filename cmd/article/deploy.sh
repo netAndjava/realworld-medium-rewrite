d3d799f6ae6589180a7959d1d86b83319fafa3ae
nqq <nqq@aozsky.com>
2021-07-13 10:44:07 +0800

    up

--- cmd/article/deploy.sh
+++ cmd/article/deploy.sh
@@ -3,10 +3,10 @@
 # 1. build
 go build -o article ~/go/src/iohttps.com/live/realworld-medium-rewrite/cmd/article/main.go
 # 2. build image
-sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/article/labs/build/build.sh
+sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/articles/labs/build/build.sh
 # 3. clean
-sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/article/labs/clean_article.sh
+sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/articles/labs/clean_article.sh
 # 4. run container and start service
-sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/article/labs/article.sh
+sh ~/go/src/iohttps.com/live/realworld-medium-rewrite/articles/labs/article.sh
 
 rm ~/go/src/iohttps.com/live/realworld-medium-rewrite/cmd/article/article
