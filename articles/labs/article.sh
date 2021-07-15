717a7e6bebb62152828562c23a3237be372aa83a
nqq <nqq@aozsky.com>
2021-07-15 14:49:33 +0800

    up

--- articles/labs/article.sh
+++ articles/labs/article.sh
@@ -12,9 +12,9 @@ podman network create $network
 podman pod create --name $pod --share net,cgroup,ipc
 
 #1. create container and run program
-podman run -dt --name $s1 --hostname $s1 --network $network --pod $pod --dns $ip_ns --dns-search "service.nqq"  hub.iohttps.com/article:dev bash
+podman run -dt --name $s1 --hostname $s1 --network $network --pod $pod --dns $ip_ns --dns-search ".service.nqq"  hub.iohttps.com/article:dev bash
 
-podman exec -dt $s1 article -db /var/etcd/mysql.toml -consul /var/etcd/consul.toml -config /var/etcd/dev.toml 
+podman exec -dt $s1 article -db /usr/local/etc/article/mysql.toml -consul /usr/local/etc/article/consul.toml -config /usr/local/etc/article/dev.toml 
+
 #2. check if article service start 
-
-podman exec -it $s1 ss -lntp
+podman exec $s1 ss -lntp
